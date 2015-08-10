5d3f4db0154753272daa8b4495c6009a4ae094cd
Mark Ryall <mark@ryall.name>
2015-08-11 00:08:00 +1000

    add fully qualified modules to view

--- web/views/card_view.ex
+++ web/views/card_view.ex
@@ -2,11 +2,11 @@ defmodule CardShark.CardView do
   use CardShark.Web, :view
 
   def render("index.json", %{cards: cards}) do
-    %{cards: render_many(cards, CardView, "card.json")}
+    %{cards: render_many(cards, CardShark.CardView, "card.json")}
   end
 
   def render("show.json", %{card: card}) do
-    render_one(card, CardView, "card.json")
+    render_one(card, CardShark.CardView, "card.json")
   end
 
   def render("card.json", %{card: card}) do
