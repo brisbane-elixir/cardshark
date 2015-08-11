94625163d660671077f0301bfd9a81fba7a37c53
Mark Ryall <mark@ryall.name>
2015-08-11 23:07:07 +1000

    remove annoying progress outputp from webpack when running in iex

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :card_shark, CardShark.Endpoint,
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
-  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--watch", "--colors", "--progress"]]
+  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--watch"]]
 
 # Watch static and templates for browser reloading.
 config :card_shark, CardShark.Endpoint,
